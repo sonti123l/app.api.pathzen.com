7c31b094146d71025babb6119baf58a0aa75fa26
SaiTrishal22 <saitrishal.s@orotron.com>
2026-03-04 12:41:04 +0530

    first api written

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,10 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import { pool } from "./db/databaseInitialization.js";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
-});
-
-app.get("/create-table", async (c) => {
-  await pool.query(`
-    CREATE TABLE pathZenStudents (
-      student_id INT AUTO_INCREMENT PRIMARY KEY,
-      student_email_id VARCHAR(255) UNIQUE NOT NULL,
-      password VARCHAR(255) NOT NULL
-    );
-  `);
-
-  return c.json({ message: "TABLE CREATED SUCCESSFULLY" });
 });
 
 serve(
